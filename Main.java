a0c7f1cd1f5f6dc2609406cd15ab6febd6ad8929
Legendary-Yurza <77443930+Legendary-Yurza@users.noreply.github.com>
2021-01-14 16:51:51 +0400

    домашнее задание

---
+++ Main.java
@@ -0,0 +1,85 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+          }
+
+
+
+
+
+// Задание 1.
+//    public static void main(String[] args) {
+//
+//    }
+
+// Задание 2.
+//        byte aByte = 127;
+//        short aShort = 32767;
+//        char aChar = 5;
+//        int aInt = 2147483647;
+//        long aLong = 1231232132;
+//        float aFloat = 145123122112.123123123f;
+//        double aDouble = 1231221.3453434;
+//        String aString = "Стринг";
+//        Boolean aBool;
+
+// Задание 3.
+//        float af = 5;
+//        float bf = 3;
+//        float cf = 4;
+//        float df = 2;
+//        double result;
+//        System.out.println(result = af * (bf + (cf / df)));
+
+//Задание 4.
+//    static boolean checkNumber (int a , int b) {
+//        int sum = a + b;
+//        if (sum >=10 && sum <= 20){
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
+
+// Задание 5
+//    static int check(int a) {
+//        if (a >= 0) {
+//            System.out.println("Число положительное!");
+//        }
+//        else {
+//            System.out.println("Чисто отрицательное!");
+//        }
+//        return 0;
+//    }
+
+//Задание 6
+//    static boolean bool (int a) {
+//    if (a >= 0) {
+//        return true;
+//        }
+//    else {
+//        return false;
+//    }
+//    }
+
+//Задание 7
+//public class Main {
+//    public static void main(String[] args) {
+//        nameS("Давид!");
+//    }
+//    public static void nameS(String name) {
+//        System.out.println("Привет, " + name);
+//            }
+//          }
+
+//Задание 8
+//    static boolean VisGod(int year) {
+//        if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0))
+//                return true;
+//        else {
+//            return false;
+//        }
+//    }
+
